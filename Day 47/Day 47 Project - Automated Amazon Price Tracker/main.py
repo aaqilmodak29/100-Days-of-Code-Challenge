6b887052fc81a291c587010cd3ba4961197281fb
Aaqil Modak <aaqil.modak53@nmims.edu.in>
2021-09-22 12:34:41 +0530

    commit

--- Day 47/Day 47 Project - Automated Amazon Price Tracker/main.py
+++ Day 47/Day 47 Project - Automated Amazon Price Tracker/main.py
@@ -6,13 +6,14 @@ from bs4 import BeautifulSoup
 URL = "https://www.amazon.com/Instant-Pot-Multi-Use-Programmable-Pressure/dp/B00FLYWNYQ/ref=bmx_dp_m06t6qvo" \
       "_1/147-2934311-5792667?pd_rd_w=uVGiM&pf_rd_p=f43599e0-aaab-4357-b62a-afc3efe44d3b&pf_rd_" \
       "r=P1HKWDRSPP1KBZVT0MCR&pd_rd_r=d884ca12-d681-44d0-b7e8-af23502c8da3&pd_rd_wg=N2N7i&pd_rd_i=B00FLYWNYQ&psc=1"
-MY_EMAIL = "studftp@gmail.com"
-PASSWORD = "th1sacc0unt1sf0rtpbr0@2002"
+MY_EMAIL = ""
+PASSWORD = ""
+
+# http://myhttpheader.com/ - visit this website for your headers
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                  "(KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36",
-    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+    "User-Agent": "",
+    "Accept-Language": ""
 }
 
 response = requests.get(url=URL, headers=headers)
@@ -29,6 +30,6 @@ if price_of_product < 80:
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(user=MY_EMAIL, password=PASSWORD)
-        connection.sendmail(from_addr=MY_EMAIL, to_addrs="aaqilmodak@gmail.com",
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs="",
                             msg="Subject:Amazon Price Alert!\n\n"
                                 f"{title_of_product} is now ${price_of_product}. Buy Now!\n{URL}")
